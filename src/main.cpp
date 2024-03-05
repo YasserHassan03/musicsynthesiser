4225e2e02d210fd1db474942ff373497b9e29e40
YHassan3 <yh@nexcomglobal.com>
2024-03-05 18:35:11 +0000

    finished lab1

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
 #include <bitset>
+#include <STM32FreeRTOS.h>
 
 // Constants
 const uint32_t interval = 100; // Display update interval
@@ -31,132 +32,55 @@ const int DEN_BIT = 3;
 const int DRST_BIT = 4;
 const int HKOW_BIT = 5;
 const int HKOE_BIT = 6;
+// struct to store system state
+struct
+{
+  std::bitset<32> inputs;
+} sysState;
 
 // Display driver object
 U8G2_SSD1305_128X32_NONAME_F_HW_I2C u8g2(U8G2_R0);
 
-const uint32_t stepSizes [] = {49977801, 54113269, 57330981, 60740013, 64351885, 68178311, 72232370, 76527532, 81078245, 85899346,91007233, 96418697}; // step sizes required for each of the 12 notes of your keyboard
+const uint32_t stepSizes[] = {49977801, 54113269, 57330981, 60740013, 64351885, 68178311, 72232370, 76527532, 81078245, 85899346, 91007233, 96418697}; // step sizes required for each of the 12 notes of your keyboard
 volatile uint32_t currentStepSize;
 
-// function to read steo size and match to key
-uint32_t readStepSize(std::bitset<32> input)
-{
-  if ((input.to_ulong() & 0x1) == 0)
-  {
-    return stepSizes[0];
-  }
-  else if ((input.to_ulong() & 0x2) == 0)
-  {
-    return stepSizes[1];
-  }
-  else if ((input.to_ulong() & 0x4) == 0)
-  {
-    return stepSizes[2];
-  }
-  else if ((input.to_ulong() & 0x8) == 0)
-  {
-    return stepSizes[3];
-  }
-  else if ((input.to_ulong() & 0x10) == 0)
-  {
-    return stepSizes[4];
-  }
-  else if ((input.to_ulong() & 0x20) == 0)
-  {
-    return stepSizes[5];
-  }
-  else if ((input.to_ulong() & 0x40) == 0)
-  {
-    return stepSizes[6];
-  }
-  else if ((input.to_ulong() & 0x80) == 0)
-  {
-    return stepSizes[7];
-  }
-  else if ((input.to_ulong() & 0x100) == 0)
-  {
-    return stepSizes[8];
-  }
-  else if ((input.to_ulong() & 0x200) == 0)
-  {
-    return stepSizes[9];
-  }
-  else if ((input.to_ulong() & 0x400) == 0)
-  {
-    return stepSizes[10];
-  }
-  else if ((input.to_ulong() & 0x800) == 0)
-  {
-    return stepSizes[11];
-  }
-  else
-  {
-    return 0;
-  }
-}
-// function that matches the step size to the note
-std::string getNote(std::bitset<32> input)
-{
-  if ((input.to_ulong() & 0x1) == 0)
-  {
-    return "C";
-  }
-  else if ((input.to_ulong() & 0x2) == 0)
-  {
-    return "C#";
-  }
-  else if ((input.to_ulong() & 0x4) == 0)
-  {
-    return "D";
-  }
-  else if ((input.to_ulong() & 0x8) == 0)
-  {
-    return "D#";
-  }
-  else if ((input.to_ulong() & 0x10) == 0)
-  {
-    return "E";
-  }
-  else if ((input.to_ulong() & 0x20) == 0)
-  {
-    return "F";
-  }
-  else if ((input.to_ulong() & 0x40) == 0)
-  {
-    return "F#";
-  }
-  else if ((input.to_ulong() & 0x80) == 0)
-  {
-    return "G";
-  }
-  else if ((input.to_ulong() & 0x100) == 0)
-  {
-    return "G#";
-  }
-  else if ((input.to_ulong() & 0x200) == 0)
-  {
-    return "A";
-  }
-  else if ((input.to_ulong() & 0x400) == 0)
-  {
-    return "A#";
-  }
-  else if ((input.to_ulong() & 0x800) == 0)
-  {
-    return "B";
-  }
-  else
-  {
-    return "no note";
-  }
-}
 // function to update the phase accumulator and set the analogue output voltage at each sample interval
 void sampleISR()
 {
+  int localStepSize;
+  __atomic_load(&currentStepSize, &localStepSize, __ATOMIC_RELAXED);
   static uint32_t phaseAcc = 0;
   phaseAcc += currentStepSize;
   int32_t Vout = (phaseAcc >> 24) - 128;
   analogWrite(OUTR_PIN, Vout + 128);
+}
+
+// function to read steo size and match to key
+uint32_t readStepSize(std::bitset<32> input)
+{
+  for (int i = 0; i < 12; ++i)
+  {
+    if ((input.to_ulong() & (1 << i)) == 0)
+    {
+      return stepSizes[i];
+    }
+  }
+  return 0;
+}
+// function that matches the step size to the note
+std::string getNote(std::bitset<32> input)
+{
+  const std::string notes[] = {
+      "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+
+  for (int i = 0; i < 12; ++i)
+  {
+    if ((input.to_ulong() & (1 << i)) == 0)
+    {
+      return notes[i];
+    }
+  }
+  return "no note";
 }
 
 // function to read inputs from the four columns of the switch matrix and store in c++ bitset
@@ -192,6 +116,46 @@ void setOutMuxBit(const uint8_t bitIdx, const bool value)
   delayMicroseconds(2);
   digitalWrite(REN_PIN, LOW);
 }
+// function for scanning the keyboard into a single function:
+void scanKeysTask(void *pvParameters)
+{
+  const TickType_t xFrequency = 50 / portTICK_PERIOD_MS;
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+  while (1)
+  {
+    vTaskDelayUntil(&xLastWakeTime, xFrequency);
+    std::bitset<32> inputs;
+    for (int i = 0; i < 3; i++)
+    {
+      setRow(i);
+      delayMicroseconds(3);
+      std::bitset<4> input = readCols();
+      inputs |= (input.to_ulong() << (i * 4));
+    }
+    sysState.inputs = inputs;
+    int localStepSize = readStepSize(sysState.inputs);
+    __atomic_store_n(&currentStepSize, localStepSize, __ATOMIC_RELAXED);
+  }
+}
+
+void displayUpdateTask(void *pvParameters)
+{
+  const TickType_t xFrequency = 100 / portTICK_PERIOD_MS;
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+  while (1)
+  {
+    vTaskDelayUntil(&xLastWakeTime, xFrequency);
+    u8g2.clearBuffer();                 // clear the internal memory
+    u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
+    u8g2.drawStr(2, 10, "Helllo World!");
+    std::string note = getNote(sysState.inputs);
+    u8g2.drawStr(2, 30, note.c_str());
+    u8g2.setCursor(2, 20);
+    u8g2.print(sysState.inputs.to_ulong(), HEX);
+    u8g2.sendBuffer(); // transfer internal memory to the display
+    digitalToggle(LED_BUILTIN);
+  }
+}
 
 void setup()
 {
@@ -224,45 +188,34 @@ void setup()
   // Initialise UART
   Serial.begin(9600);
   Serial.println("Hello World");
-  //timer to trigger interrupt to call sampleISR()
+  // timer to trigger interrupt to call sampleISR()
   TIM_TypeDef *Instance = TIM1;
   HardwareTimer *sampleTimer = new HardwareTimer(Instance);
   sampleTimer->setOverflow(22000, HERTZ_FORMAT);
   sampleTimer->attachInterrupt(sampleISR);
   sampleTimer->resume();
+  //multithreading
+  TaskHandle_t scanKeysHandle = NULL;
+  xTaskCreate(
+      scanKeysTask,     /* Function that implements the task */
+      "scanKeys",       /* Text name for the task */
+      64,               /* Stack size in words, not bytes */
+      NULL,             /* Parameter passed into the task */
+      1,                /* Task priority */
+      &scanKeysHandle); /* Pointer to store the task handle */
+  TaskHandle_t displayUpdateHandle = NULL;
+  xTaskCreate(
+      displayUpdateTask, /* Function that implements the task */
+      "displayUpdate",  /* Text name for the task */
+      64,               /* Stack size in words, not bytes */
+      NULL,             /* Parameter passed into the task */
+      1,                /* Task priority */
+      &displayUpdateHandle); /* Pointer to store the task handle */
+  vTaskStartScheduler();
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  static uint32_t next = millis();
-  static uint32_t count = 0;
-  std::bitset<32> inputs;
-
-  while (millis() < next); // Wait for next interval
-
-  next += interval;
-
-  // Update display
-  u8g2.clearBuffer();                 // clear the internal memory
-  u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
-  u8g2.drawStr(2, 10, "Helllo World!");
-  for (int i = 0; i < 3; i++)
-  {
-    setRow(i);
-    delayMicroseconds(3);
-    std::bitset<4> input = readCols();
-    inputs |= (input.to_ulong() << (i * 4));
-  } // write something to the internal memory
-  currentStepSize = readStepSize(inputs);
-  std::string note = getNote(inputs);
-  u8g2.drawStr(2, 30, note.c_str());
-
-  // std::bitset<4> inputs = readCols();
-  u8g2.setCursor(2, 20);
-  u8g2.print(inputs.to_ulong(), HEX);
-  u8g2.sendBuffer(); // transfer internal memory to the display
-
-  // Toggle LED
-  digitalToggle(LED_BUILTIN);
+  
 }
