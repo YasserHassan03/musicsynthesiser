d5c97820431eef29eac899ff37e8af47845800fa
yayyzan <yazayyoub@gmail.com>
2024-03-05 21:50:19 +0000

    Lab1 completed - may need future mods

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
+#include <bitset>
+#include <string>
+#include <iostream>
+#include <STM32FreeRTOS.h>
 
 //Constants
   const uint32_t interval = 100; //Display update interval
+  const uint32_t stepSizes [] = {49977801, 54113269, 57330981, 60740013, 64351885, 68178311, 72232370, 76527532, 81078245, 85899346, 91007233, 96418697};
+  const std::string notes [] = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+  volatile uint32_t currentStepSize;
+  std::string note = "No note detected";
+  struct {std::bitset<32> inputs;} sysState;
 
 //Pin definitions
   //Row select and enable
@@ -47,8 +56,100 @@ void setOutMuxBit(const uint8_t bitIdx, const bool value) {
       digitalWrite(REN_PIN,LOW);
 }
 
+std::bitset<4> readCols(){
+  std::bitset<4> result;
+  result[0] = digitalRead(C0_PIN);
+  result[1] = digitalRead(C1_PIN);
+  result[2] = digitalRead(C2_PIN);
+  result[3] = digitalRead(C3_PIN);
+  return result;
+}
+
+void setRow(uint8_t rowIdx){
+  digitalWrite(REN_PIN,LOW);
+  digitalWrite(RA0_PIN, rowIdx & 0x01);
+  digitalWrite(RA1_PIN, rowIdx & 0x02);
+  digitalWrite(RA2_PIN, rowIdx & 0x04);
+  digitalWrite(REN_PIN,HIGH);
+}
+
+void scanKeysTask(void *pvParameters){
+  const TickType_t xFrequency = 50/portTICK_PERIOD_MS;
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+
+  while (1) {
+    vTaskDelayUntil( &xLastWakeTime, xFrequency );
+    uint32_t localCurrentStepSize;
+    sysState.inputs.reset();
+    for (int i = 0; i<3; i++){
+      setRow(i);
+      delayMicroseconds(3);
+      sysState.inputs |= (readCols().to_ulong() << (4 * i));
+    }
+    if (sysState.inputs != 0xFFF) {
+      int index = static_cast<int>(std::log2((0xFFF - sysState.inputs.to_ulong())));
+      localCurrentStepSize = stepSizes[index];
+      note = notes[index];
+    }
+    else {  
+      localCurrentStepSize = 0;
+      note = "No note detected";
+    }
+    __atomic_store_n(&currentStepSize, localCurrentStepSize, __ATOMIC_RELAXED);
+  }
+}
+
+void displayUpdate(void *pvParameters){
+  const TickType_t xFrequency = 100/portTICK_PERIOD_MS;
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+
+  while (1) {
+    vTaskDelayUntil( &xLastWakeTime, xFrequency );
+    u8g2.clearBuffer();        
+    u8g2.setFont(u8g2_font_ncenB08_tr); 
+    u8g2.drawStr(2,10,"Stack Synthesizer!!");  
+    u8g2.setCursor(2,20);
+    u8g2.print(sysState.inputs.to_ulong(),HEX);  
+    u8g2.drawStr(2, 30, note.c_str());
+    u8g2.sendBuffer();          
+    digitalToggle(LED_BUILTIN);
+  }
+}
+
+void sampleISR(){
+  static uint32_t phaseAcc = 0;
+  uint32_t localCurrentStepSize = 0;
+  __atomic_load(&currentStepSize, &localCurrentStepSize, __ATOMIC_RELAXED);
+  phaseAcc += localCurrentStepSize;
+  int32_t Vout = (phaseAcc >> 24) - 128;
+  analogWrite(OUTR_PIN, Vout + 128);
+}
+
 void setup() {
   // put your setup code here, to run once:
+  TIM_TypeDef *Instance = TIM1;
+  HardwareTimer *sampleTimer = new HardwareTimer(Instance);
+  sampleTimer->setOverflow(22000, HERTZ_FORMAT);
+  sampleTimer->attachInterrupt(sampleISR);
+  sampleTimer->resume();
+
+  TaskHandle_t scanKeysHandle = NULL;
+  xTaskCreate(
+  scanKeysTask,		
+  "scanKeys",		
+  64,      		
+  NULL,			
+  1,			
+  &scanKeysHandle);	
+  
+  TaskHandle_t displayUpdateHandle = NULL;
+  xTaskCreate(
+  displayUpdate,		
+  "displayUpdate",		
+  64,      		
+  NULL,			
+  2,			
+  &displayUpdateHandle);	
 
   //Set pin directions
   pinMode(RA0_PIN, OUTPUT);
@@ -77,26 +178,9 @@ void setup() {
   //Initialise UART
   Serial.begin(9600);
   Serial.println("Hello World");
+  vTaskStartScheduler();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  static uint32_t next = millis();
-  static uint32_t count = 0;
 
-  while (millis() < next);  //Wait for next interval
-
-  next += interval;
-
-  //Update display
-  u8g2.clearBuffer();         // clear the internal memory
-  u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
-  u8g2.drawStr(2,10,"Helllo World!");  // write something to the internal memory
-  u8g2.setCursor(2,20);
-  u8g2.print(count++);
-  u8g2.sendBuffer();          // transfer internal memory to the display
-
-  //Toggle LED
-  digitalToggle(LED_BUILTIN);
-  
 }
